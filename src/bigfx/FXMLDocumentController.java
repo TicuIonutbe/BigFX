2ee9d4b3843a008b387f161a47d1df1634b00fb3
nmcristian <nmcristian@gmail.com>
2018-11-22 23:12:48 +0100

    initial commit

---
+++ src/bigfx/FXMLDocumentController.java
@@ -0,0 +1,264 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigfx;
+
+import java.awt.Component;
+import java.awt.Graphics;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ticu
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private TextArea txtDesc;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtSecondName;
+    @FXML
+    private TextField txtAge;
+    @FXML
+    private TextField txtGender;
+    @FXML
+    private TextField txtFamilyStatus;
+    @FXML
+    private TextField txtKids;
+    @FXML
+    private ImageView imgImageProfile;
+    @FXML
+    private CheckBox chkNewsletter;
+    @FXML
+    private RadioButton rbtUnemploymentNo;
+    @FXML
+    private ToggleGroup unemployment;
+    @FXML
+    private RadioButton rbtUnemploymentYes;
+    @FXML
+    private Spinner nudCars;
+    @FXML
+    private ComboBox cboFootbal;
+    @FXML
+    private ListView lstInterests;
+    @FXML
+    private Button btnButton;
+    @FXML
+    private Label lblInvisible;
+    @FXML
+    private Button btnFakeData;
+    Image img1;
+    Image img2;
+    Image img3;
+    Optional<ButtonType> result;
+    Alert alert;
+    @FXML
+    private Button btnExit;
+    TextInputDialog inputbox;
+    List<Image> ImagesList;
+    @FXML
+    public ListView<User> lstClients;
+    @FXML
+    private Button btnViewClients;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Inputbox
+        inputbox = new TextInputDialog();
+        inputbox.setTitle("Close program confirmation");
+        inputbox.setHeaderText("Write Yes if you want to close the software");
+        inputbox.setContentText("Answer:");
+        //Info Alert
+        JOptionPane.showMessageDialog(null, "Program will start.", "Status of program:", JOptionPane.INFORMATION_MESSAGE);
+        //Alert
+        alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Look, a Confirmation Dialog");
+        alert.setContentText("Are you ok with this?");
+
+        //Initialize images
+        img1 = new Image("/images/1.jpg");
+        img2 = new Image("/images/2.jpg");
+        img3 = new Image("/images/3.jpg");
+
+        ImagesList = new ArrayList<Image>();
+
+        ImagesList.add(img1);
+        ImagesList.add(img2);
+        ImagesList.add(img3);
+
+        nudCars.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100, 0, 1));
+        //nudCars.getValue().toString()
+
+        cboFootbal.getItems().addAll("Steaua", "Dinamo", "Rapid", "Cluj");
+        //cboFootbal.getValue().toString()
+
+        lstInterests.getItems().addAll("Sport", "Footbal", "Sex", "Movies", "Swimming", "Going Out");
+        //lstInterests.getSelectionModel().getSelectedItem().toString() for 1 item selected
+        lstInterests.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        //lstInterests.getSelectionModel().getSelectedItems() for multiple selections.
+
+        //for (Object selectedItem : lstInterests.getSelectionModel().getSelectedItems()) {
+        //             System.out.println(selectedItem);
+        //}
+    }
+
+    @FXML
+    private void mouse_clicked(MouseEvent event) {
+        result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            String Radio = ((RadioButton) unemployment.getSelectedToggle()).getText();
+            User t = new User(txtName.getText(), txtSecondName.getText(), Integer.valueOf(txtAge.getText()),
+                    txtGender.getText(), txtFamilyStatus.getText(), txtKids.getText(),
+                    txtDesc.getText(), imgImageProfile.getImage(), chkNewsletter.isSelected(), Radio,
+                    (int) nudCars.getValue(), cboFootbal.getValue().toString(), lstInterests.getSelectionModel().getSelectedItems());
+            lstClients.getItems().add(t);
+            lstClients.setCellFactory(new Callback<ListView<User>, ListCell<User>>() {
+                @Override
+                public ListCell<User> call(ListView<User> p) {
+                    ListCell<User> cell = new ListCell<User>() {
+                        @Override
+                        protected void updateItem(User t, boolean bln) {
+                            super.updateItem(t, bln);
+                            if (t != null) {
+                                setText(t.name);
+                            }
+                        }
+                    };
+                    return cell;
+                }
+            });
+
+        } else {
+            System.out.println("Cancel was pressed.");
+
+        }
+
+    }
+
+    @FXML
+    private void fake_data(MouseEvent event) {
+        txtName.setText("Ticu");
+        txtSecondName.setText("Ionut");
+        txtAge.setText("29");
+        txtGender.setText("Male");
+        txtFamilyStatus.setText("Married");
+        txtKids.setText("1 Daughter");
+        txtDesc.setText("I am a trust worthy person on which u can rely.");
+        chkNewsletter.setSelected(true);
+        rbtUnemploymentNo.setSelected(true);
+        cboFootbal.getSelectionModel().select(0);
+    }
+
+    @FXML
+    private void change_image(MouseEvent event) {
+        Random iRandom = new Random();
+        int r1 = iRandom.nextInt(3);
+        if (r1 == 0) {
+            imgImageProfile.setImage(ImagesList.get(0));
+        } else if (r1 == 1) {
+            imgImageProfile.setImage(ImagesList.get(1));
+        } else {
+            imgImageProfile.setImage(ImagesList.get(2));
+        }
+
+    }
+
+    @FXML
+    private void exit_software(MouseEvent event) {
+        String exit = "";
+        Optional<String> sResultaat = inputbox.showAndWait();
+        sResultaat.ifPresent(sText -> {
+            if (sText.toLowerCase().equals("yes")) {
+                System.out.println("System closed.");
+                System.exit(0);
+            }
+        });
+
+    }
+
+    private ImageIcon createImageIcon(String path,
+            String description) {
+        java.net.URL imgURL = getClass().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    private User getUserByName(String name) {
+        for (User u : User.UserList) {
+            if (u.name.toLowerCase().equals(name.toLowerCase())) {
+                return u;
+            }
+        }
+        return null;
+    }
+
+    @FXML
+    private void view_clients(MouseEvent event) {
+        User CurrentUser = lstClients.getSelectionModel().getSelectedItem();
+
+        String InfoClient = "Nume: " + CurrentUser.name + "\n"
+                + "Prenume: " + CurrentUser.secondname + "\n"
+                + "Varsta: " + CurrentUser.age + "\n"
+                + "Gender: " + CurrentUser.gender + "\n"
+                + "Status Familial: " + CurrentUser.family_status + "\n"
+                + "Copii: " + CurrentUser.kids + "\n"
+                + "Description: " + CurrentUser.desc + "\n"
+                + "Image: " + CurrentUser.img + "\n"
+                + "Newsletter: " + CurrentUser.newsletter + "\n"
+                + "Unemployed:" + CurrentUser.unemployment + "\n"
+                + "Cars: " + CurrentUser.nrcars + "\n"
+                + "Favorite Team: " + CurrentUser.favteam + "\n"
+                + "Interests: " + CurrentUser.interests.toString() + "\n"
+                + "";
+
+//        https://stackoverflow.com/questions/6714045/how-to-resize-jlabel-imageicon
+//        ImageIcon imageIcon = new ImageIcon(new ImageIcon("icon.png").getImage().getScaledInstance(20, 20, Image.SCALE_DEFAULT));
+        ImageIcon icon = createImageIcon("/images/jaa.jpg", "a pretty but meaningless splat");
+        icon = new ImageIcon(icon.getImage().getScaledInstance(100, 100, java.awt.Image.SCALE_DEFAULT));
+        JOptionPane.showMessageDialog(null, InfoClient, "Status of program:", JOptionPane.INFORMATION_MESSAGE, icon);
+
+//        this works if img3 would be declared as kavax.swing.Image
+//        img3 = img3.getScaledInstance(100, 100, java.awt.Image.SCALE_DEFAULT);
+//        JOptionPane.showMessageDialog(null, InfoClient, "Status of program:", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(img3));
+    }
+
+}
---
+++ src/bigfx/User.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bigfx;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author Ticu
+ */
+public class User {
+    
+    public String name;
+    public String secondname;
+    public int age;
+    public String gender;
+    public String family_status;
+    public String kids;
+    public String desc;
+    public Image img;
+    public boolean newsletter;
+    public String unemployment;
+    public int nrcars;
+    public String favteam;
+    public ObservableList interests;
+    public static List<User> UserList = new ArrayList<User>();
+    
+    public User(String name, String secondname, int age, String gender, String familystatus, String kids,
+            String desc,Image img, boolean newsletter, String unemployment, int cars, String favteam, ObservableList list){
+    this.name=name;
+    this.secondname=secondname;
+    this.age=age;
+    this.gender=gender;
+    this.family_status=familystatus;
+    this.kids=kids;
+    this.desc=desc;
+    this.img=img;
+    this.newsletter=newsletter;
+    this.unemployment=unemployment;
+    this.nrcars=cars;
+    this.favteam=favteam;
+    this.interests=list;
+        UserList.add(this);
+        for (User user : UserList) {
+            System.out.print(user.name + " ");
+        } System.out.println("End List");
+    
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "name=" + name + ", secondname=" + secondname + ", age=" + age + ", gender=" + gender + ", family_status=" + family_status + ", kids=" + kids + ", desc=" + desc + ", img=" + img + ", newsletter=" + newsletter + ", unemployment=" + unemployment + ", nrcars=" + nrcars + ", favteam=" + favteam + ", interests=" + interests + '}';
+    }
+    
+    public String toStringForThatBox() {
+        return "User{" + "name=" + name + ", secondname=" + secondname + ", age=" + age + ", gender=" + gender + ", family_status=" + family_status + ", kids=" + kids + ", desc=" + desc + ", img=" + img + ", newsletter=" + newsletter + ", unemployment=" + unemployment + ", nrcars=" + nrcars + ", favteam=" + favteam + ", interests=" + interests + '}';
+    }
+    
+}
+
+
